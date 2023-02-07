4bfac1f0bd7fe0fb91f5fa9f9262ba3c13f5350a
akshay tuptewar <81164399+akshaytuptewar19@users.noreply.github.com>
2023-02-07 18:57:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
-const cors = require("cors");
+const cors = require("cors");//Cross-origin resource sharing (CORS) is a browser security feature restricts http request.
 
 app.use(cors());
 app.use(express.json());
