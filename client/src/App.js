e95bbf2ecea6310f0aa9bf9a9dcd358d22d7c9ef
akshay tuptewar <81164399+akshaytuptewar19@users.noreply.github.com>
2023-02-08 19:11:53 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import Axios from "axios";
+import Axios from "axios"; //great library used to access public apis
 
 function App() {
   const [name, setName] = useState("");
